37a99bcce92128b078ba04e67df8864b9be32b6a
Steve-Krug <34281972+Steve-Krug@users.noreply.github.com>
2021-07-13 13:23:15 -0700

    New due firmware

--- 01-Firmware/Arduino/Arduino DUE/mpu_6050_read_v2/mpu_6050_read_v2.ino
+++ 01-Firmware/Arduino/Arduino DUE/mpu_6050_read_v2/mpu_6050_read_v2.ino
@@ -1,10 +1,35 @@
 #include <Wire.h>
+#include <SPI.h> // Include the SPI Library
+#include <SD.h>
 
 int INS = 0x68; // MPU6050 I2C address
 int16_t Gyro_X, Gyro_Y, Gyro_Z;
 float P, Q, R;
+int pinCS = 4;
 
 void setup() {
+
+  // I2Cdev and MPU6050 must be installed as libraries, or else the .cpp/.h files
+  // for both classes must be in the include path of your project
+#include "I2Cdev.h"
+
+#include "MPU6050_6Axis_MotionApps20.h"
+  //#include "MPU6050.h" // not necessary if using MotionApps include file
+
+  // Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
+  // is used in I2Cdev.h
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+#include "Wire.h"
+#endif
+
+  // class default I2C address is 0x68
+  // specific I2C addresses may be passed as a parameter here
+  // AD0 low = 0x68 (default for SparkFun breakout and InvenSense evaluation board)
+  // AD0 high = 0x69
+  MPU6050 mpu;
+  //MPU6050 mpu(0x69); // <-- use for AD0 high
+
+
   Serial.begin(9600);
   Wire.begin();                      // Initialize comunication
   Wire.beginTransmission(INS);       // Start communication with MPU6050 // INS=0x68
@@ -12,6 +37,16 @@ void setup() {
   Wire.write(0);                     // Make reset - place a 0 into the 6B register
   Wire.endTransmission(true);        // End the transmission
 
+  //pinMode(pinCS, OUTPUT);
+  //SPI.begin();
+
+  if (SD.begin(pinCS))
+  {
+    Serial.println("SD card is ready to use.");
+  } else {
+    Serial.println("SD card intialization failed.");
+    return;
+  }
 
 }
 void loop() {
